b5da3543c8f05305907ddb36a36c855761eb0edf
soumyasn <nagabandisoumyasree24047@gmail.com>
2023-11-08 22:58:00 +0530

    deleteTodoAPI and retrieveTodoAPI

--- todo-app/src/components/Todo/ListTodoComponent.jsx
+++ todo-app/src/components/Todo/ListTodoComponent.jsx
@@ -1,27 +1,41 @@
 import { useState, useEffect } from "react";
-import { retrieveAllTodosForUsername } from "../api/TodoApiService";
+import { deleteTodoApi, retrieveAllTodosForUsernameApi } from "../api/TodoApiService";
 export default function ListTodoComponent(){
-    const today = new Date();
     const [todos, setTodos] = useState([])
+    const [message, setMessage] = useState("")
     useEffect (
         () => refershTodos()
     )
     function refershTodos() {
-    retrieveAllTodosForUsername("soumya")
+    retrieveAllTodosForUsernameApi("soumya")
     .then( response => setTodos(response.data))
     .catch(error => console.log(error))
+    }
+
+    function deleteTodo(id){
+        console.log('clicked' + id)
+        deleteTodoApi('soumya',id)
+        .then(
+            //1: Display Message
+            () => {
+            setMessage(`Delete of todo with ${id} is Successfull`)
+            refershTodos()
+            }
+            //2: Update Todos List
+        )
+        .catch()
     }
     return(
         <div className="container">
             <h1> Things you want to Do!</h1>
+            {message && <div className="alert alert-warning">{message}</div>}
             <div>
             <table className="table">
                     <thead>
                         <tr>
-                            <td>id</td>
-                            <td>description</td>
-                            <td>done</td>
-                            <td>targetDate</td>
+                            <th>description</th>
+                            <th>done</th>
+                            <th>targetDate</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -29,10 +43,11 @@ export default function ListTodoComponent(){
                         todos.map(
                             todo => (
                             <tr key = "{todo.id}">
-                                    <td>{todo.id}</td>
+        
                                     <td>{todo.description}</td>
                                     <td>{todo.done.toString()}</td>
                                     <td>{todo.targetDate.toString()}</td>
+                                    <td><button className="btn btn-warning" onClick={()=>deleteTodo(todo.id)}>Delete</button></td>
                                 </tr>
                             )
                         )
--- todo-app/src/components/api/TodoApiService.js
+++ todo-app/src/components/api/TodoApiService.js
@@ -1,3 +1,11 @@
 import  axios  from "axios"
-export const retrieveAllTodosForUsername = (username) =>  
-axios.get(`http://localhost:8086/users/${username}/todos`)
+
+const appClient = axios.create (
+    {
+        baseURL : 'http://localhost:8082'
+    }
+)
+export const retrieveAllTodosForUsernameApi = (username) =>  
+appClient.get(`/users/${username}/todos`)
+export const deleteTodoApi =(username, id) =>
+appClient.delete(`/users/${username}/todos/${id}`)
